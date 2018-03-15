76ba748aaaa33e18ce38dfde458f3add1ab8555b
byzg <byzg00@gmail.com>
2018-03-15 13:23:02 +0300

    Update builder.rb

--- lib/translate_enum/builder.rb
+++ lib/translate_enum/builder.rb
@@ -25,11 +25,11 @@ module TranslateEnum
     end
 
     def i18n_key
-      @i18n_key ||= "#{attribute}_list"
+      @i18n_key ||= attribute
     end
 
     def i18n_location(key)
-      "#{model.model_name.i18n_key}.#{i18n_key}.#{key}"
+      "#{model.model_name.i18n_key}/#{i18n_key}.#{key}"
     end
 
     def i18n_default_location(key)
